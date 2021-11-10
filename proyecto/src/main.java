9f624a503083661f972588e103256aecb5fd352c
Alberto <albertowiltrong@gmail.com>
2021-11-10 12:01:37 +0100

    Initial commit

---
+++ proyecto/src/main.java
@@ -0,0 +1,29 @@
+public class main { 
+
+    public static void main(String[] args){
+        saludoBucle();
+        despedida();
+    }
+
+
+    /*
+    Muestra hola mundo 5 veces
+    de una manera bastatnte ineficiente.    
+    */
+
+    public static void main saludoBucle(){
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+    }
+
+    /*
+    Muestra un mensaje que infica el
+    final de la ejecución del programa.
+     */
+
+    private static void despedida() { System.out.println("Fin del programa.")}
+
+}
